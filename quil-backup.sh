527cd934a9151824cc3c806d80b0ed93e714eecf
fyw <15651990091@163.com>
2024-06-09 13:39:03 +0800

    sh

--- quil-backup.sh
+++ quil-backup.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
+
+mkdir -p ~/backup-quil
 # Create the backup script
-sudo cat <<EOF >"$USER_HOME/backup_script.sh"
+sudo cat <<EOF >"~/backup_script.sh"
 #!/bin/bash
 # Get the current date in YYYYMMDD format
 current_date=$(date +%Y%m%d)
 
 # Define the tar file name using current date and peer_id
-TAR_FILE="$USER_HOME/${current_date}_quil_store_backup.tar.gz"
+TAR_FILE="~/backup-quil/${current_date}_quil_store_backup.tar.gz"
 
 # Define the directory to be tarred
-DIR_TO_TAR="$USER_HOME/ceremonyclient/node/.config"
+DIR_TO_TAR="~/ceremonyclient/node/.config"
 
 # Create a tar.gz file of the specified directory
 echo "Creating tar file of \$DIR_TO_TAR..."
@@ -25,19 +27,18 @@ echo "Script execution completed."
 EOF
 
 # Make the backup script executable
-sudo chmod +x $USER_HOME/backup_script.sh
+sudo chmod +x ~/backup_script.sh
 
 # Check if cron job exists
-existing_cron=$(crontab -l | grep "$USER_HOME/backup_script.sh")
+existing_cron=$(crontab -l | grep "~/backup_script.sh")
 
-sudo chmod +x $USER_HOME/backup_script.sh
-$USER_HOME/backup_script.sh
+sudo chmod +x ~/backup_script.sh
+~/backup_script.sh
 
 # Schedule the backup script if it's not already scheduled
 if [ -z "$existing_cron" ]; then
-    (crontab -l 2>/dev/null; echo "0 13 * * * $USER_HOME/backup_script.sh") | crontab -
-    echo "Backup script scheduled to run daily at 13:00."
+    (crontab -l 2>/dev/null; echo "50 13 * * * ~/backup_script.sh") | crontab -
+    echo "Backup script scheduled to run daily at 13:50."
 else
     echo "Backup script already scheduled."
 fi
-
