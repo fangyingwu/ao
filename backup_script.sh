3777b5a49b66a126732aabe629bddc330c143603
fyw <15651990091@163.com>
2024-06-09 15:57:36 +0800

    sh

--- backup_script.sh
+++ backup_script.sh
@@ -1,16 +1,19 @@
-#!/bin/bash
-TAR_FILE=$(date +%Y%m%d)"_quil_store_backup.tar.gz"
+##!/bin/bash
+#TAR_FILE=$(date +%Y%m%d)"_quil_store_backup.tar.gz"
+#
+#DIR_TO_TAR=$HOME"/ceremonyclient/node/.config"
+#
+#mkdir ~/backup-quil
+#cd ~/backup-quil
+#echo "Creating tar file of $DIR_TO_TAR..."
+#sudo tar -zcvf $TAR_FILE $DIR_TO_TAR
+#if [ $? -eq 0 ]; then
+#    echo "Tar file created successfully: $TAR_FILE"
+#else
+#    echo "Error creating tar file" >&2
+#    exit 1
+#fi
+#echo "Script execution completed."
+mkdir -p  ~/backup-quil
+cp -r ~/ceremonyclient/node/.config  ~/backup-quil/config_$(date +%Y%m%d)
 
-DIR_TO_TAR=$HOME"/ceremonyclient/node/.config"
-
-mkdir ~/backup-quil
-cd ~/backup-quil
-echo "Creating tar file of $DIR_TO_TAR..."
-sudo tar -zcvf $TAR_FILE $DIR_TO_TAR
-if [ $? -eq 0 ]; then
-    echo "Tar file created successfully: $TAR_FILE"
-else
-    echo "Error creating tar file" >&2
-    exit 1
-fi
-echo "Script execution completed."
