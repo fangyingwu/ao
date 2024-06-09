3764677536eca81437be632bedadcf2ea1d5e505
fyw <15651990091@163.com>
2024-06-09 16:08:53 +0800

    sh

--- quil-backup.sh
+++ quil-backup.sh
@@ -9,8 +9,8 @@ existing_cron=$(crontab -l | grep "~/backup_script.sh")
 
 # Schedule the backup script if it's not already scheduled
 if [ -z "$existing_cron" ]; then
-    (crontab -l 2>/dev/null; echo "10 16 * * * ~/backup_script.sh") | crontab -
-    echo "Backup script scheduled to run daily at 16:10."
+    (crontab -l 2>/dev/null; echo "10 17 * * * ~/backup_script.sh") | crontab -
+    echo "Backup script scheduled to run daily at 17:10."
 else
     echo "Backup script already scheduled."
 fi
