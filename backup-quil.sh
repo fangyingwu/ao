978927aebcefe9a72fe1507a1333910e2e21ec76
fyw <15651990091@163.com>
2024-06-09 13:11:31 +0800

    sh

--- backup-quil.sh
+++ backup-quil.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo mkdir -p ~/backup-quil
 
-echo "0 0 * * * sudo cp -r ~/ceremonyclient/node/.config  ~/backup-quil/config_$(date +%Y%m%d)" | crontab -
+echo "15 13 * * * sudo cp -r ~/ceremonyclient/node/.config  ~/backup-quil/config_$(date +%Y%m%d)" | crontab -
