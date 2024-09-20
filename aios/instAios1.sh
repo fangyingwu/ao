4e2e69ef4d7b02985acb6d8866124abbdb79551d
fyw <15651990091@163.com>
2024-09-20 09:53:14 +0800

    feat1.aios install

--- aios/instAios1.sh
+++ aios/instAios1.sh
@@ -8,6 +8,8 @@ sleep 1
 
 nohup aios-cli start >> aios.log 2>&1 &
 
+source  /home/ubuntu/.bashrc
+
 aios-cli models add hf:TheBloke/Mistral-7B-Instruct-v0.1-GGUF:mistral-7b-instruct-v0.1.Q4_K_S.gguf
 
 
