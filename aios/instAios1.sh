e2ad42f189b626c271effc62b512cee08cf3c58e
fyw <15651990091@163.com>
2024-09-20 09:20:06 +0800

    feat1.aios install

--- aios/instAios1.sh
+++ aios/instAios1.sh
@@ -4,6 +4,10 @@ curl https://download.hyper.space/api/install | bash
 
 source  /home/ubuntu/.bashrc
 
-reboot
+sleep 1
+
+nohup aios-cli start >> aios.log 2>&1 &
+
+aios-cli models add hf:TheBloke/Mistral-7B-Instruct-v0.1-GGUF:mistral-7b-instruct-v0.1.Q4_K_S.gguf
 
 
