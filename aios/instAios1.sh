d3e0ac0b6442d6c12142552c5ecd4157a8ca097b
fyw <15651990091@163.com>
2024-09-23 18:18:48 +0800

    feat1.aios install

--- aios/instAios1.sh
+++ aios/instAios1.sh
@@ -4,12 +4,18 @@ curl https://download.hyper.space/api/install | bash
 
 source  /home/ubuntu/.bashrc
 
-sleep 1
-
 nohup aios-cli start >> aios.log 2>&1 &
 
-source  /home/ubuntu/.bashrc
+sleep 5
 
 aios-cli models add hf:TheBloke/Mistral-7B-Instruct-v0.1-GGUF:mistral-7b-instruct-v0.1.Q4_K_S.gguf
+
+aios-cli hive login
+
+aios-cli hive import-keys .config/hyperspace/key.pem
+
+aios-cli hive login
+
+aios-cli hive connect
 
 
