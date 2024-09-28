d13db50c408f8bfa22b7bdcbc91b11a33cc04dff
fyw <15651990091@163.com>
2024-09-28 21:00:49 +0800

    feat1.aios install

--- aios/instAios1.sh
+++ aios/instAios1.sh
@@ -8,14 +8,14 @@ nohup ./home/ubuntu/.aios/aios-cli start >> aios.log 2>&1 &
 
 sleep 5
 
-./home/ubuntu/.aios/aios-cli models add hf:TheBloke/Mistral-7B-Instruct-v0.1-GGUF:mistral-7b-instruct-v0.1.Q4_K_S.gguf
+/home/ubuntu/.aios/aios-cli models add hf:TheBloke/Mistral-7B-Instruct-v0.1-GGUF:mistral-7b-instruct-v0.1.Q4_K_S.gguf
 
-./home/ubuntu/.aios/aios-cli hive login
+/home/ubuntu/.aios/aios-cli hive login
 
-./home/ubuntu/.aios/aios-cli hive import-keys .config/hyperspace/key.pem
+/home/ubuntu/.aios/aios-cli hive import-keys .config/hyperspace/key.pem
 
-./home/ubuntu/.aios/aios-cli hive login
+/home/ubuntu/.aios/aios-cli hive login
 
-./home/ubuntu/.aios/aios-cli hive connect
+/home/ubuntu/.aios/aios-cli hive connect
 
 
