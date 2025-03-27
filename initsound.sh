b86e296ef30a344b7fb763af17711700474722da
fangyingwu <35124617+fangyingwu@users.noreply.github.com>
2025-03-27 14:48:59 +0800

    Update initsound.sh

--- initsound.sh
+++ initsound.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-sudo DEBIAN_FRONTEND=noninteractive apt install -y pkg-config libssl-dev
-
-sudo DEBIAN_FRONTEND=noninteractive apt install -y expect
-
-sudo NEEDRESTART_MODE=a apt update
-
+sudo DEBIAN_FRONTEND=noninteractive apt install -y expect && sudo DEBIAN_FRONTEND=noninteractive apt install -y pkg-config libssl-dev && sudo NEEDRESTART_MODE=a apt update && \
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y && \
 source $HOME/.cargo/env && rustc --version && cargo --version && echo 'source $HOME/.cargo/env' >> ~/.bashrc && source ~/.bashrc && \
 curl -sSL https://raw.githubusercontent.com/soundnesslabs/soundness-layer/main/soundnessup/install | bash && \
 source ~/.bashrc && soundnessup install && \
-soundnessup update
+soundnessup update 
 
 expect <<EOF
 spawn soundness-cli generate-key --name my-key
